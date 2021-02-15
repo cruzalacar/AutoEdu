24768653a69aa695c4514982f59015f994472511
jawedzak <jawedzak@sheridancollege.ca>
2021-02-15 15:51:28 -0500

    - Added buttons - Added Loading page (Splash screen) - Added navigation - Added layout files

---
+++ app/src/main/java/sheridan/jawedzak/autoedu/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package sheridan.jawedzak.autoedu
+
+import android.content.Intent
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val lod = findViewById<ImageView>(R.id.iv_note)
+
+        lod.alpha = 0f
+        lod.animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
+    }
+}
