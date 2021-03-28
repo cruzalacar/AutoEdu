f7333f8966b38da71a829d7aadb90b8b32efbb90
jawedzak <jawedzak@sheridancollege.ca>
2021-03-28 19:11:27 -0400

    - Fixed Recycler Detail page

--- app/src/main/java/sheridan/jawedzak/autoedu/dashLightSymbols/SymbolDetail.kt
+++ app/src/main/java/sheridan/jawedzak/autoedu/dashLightSymbols/SymbolDetail.kt
@@ -13,9 +13,21 @@ class SymbolDetail  : AppCompatActivity(){
         setContentView(R.layout.symbol_detail)
 
         var name = intent.getStringExtra("name")
+        var icon = intent.getStringExtra("icon")
+        var description = intent.getStringExtra("description")
+        var trigger = intent.getStringExtra("trigger")
+        var solution = intent.getStringExtra("solution")
+
 
         val lblName = findViewById<TextView>(R.id.name)
+        val lblDescription = findViewById<TextView>(R.id.description)
+        val lblTrigger = findViewById<TextView>(R.id.trigger)
+        val lblSolution = findViewById<TextView>(R.id.solution)
+
         lblName.text = name
+        lblDescription.text = description
+        lblTrigger.text = trigger
+        lblSolution.text = solution
     }
 
 
