d0907f6728981b81464527b9b6b8455190d5fceb
Arceline Cruz <cruzalacar@outlook.com>
2021-04-07 16:49:03 -0400

    updating SearchFragment and AssistantFragment

--- app/src/main/java/sheridan/jawedzak/autoedu/bottomNavbar/AssistantFragment.kt
+++ app/src/main/java/sheridan/jawedzak/autoedu/bottomNavbar/AssistantFragment.kt
@@ -1,60 +1,154 @@
 package sheridan.jawedzak.autoedu.bottomNavbar
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_bot.*
+import kotlinx.coroutines.*
 import sheridan.jawedzak.autoedu.R
+import sheridan.jawedzak.autoedu.chatBot.Message
+import sheridan.jawedzak.autoedu.chatBot.MessagingAdapter
+import sheridan.jawedzak.autoedu.utils.BotResponse
+import sheridan.jawedzak.autoedu.utils.Constants
+import sheridan.jawedzak.autoedu.utils.Time
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AssistantFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AssistantFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    //calling message from data list
+    var messagesList = mutableListOf<Message>()
+    //message adapter to retrieve messages
+    private lateinit var adapter: MessagingAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+        //first line when user opens the chat
+        customBotMessage("Hello, welcome to AutoEDU how may I help you today?")
+
+        //send message button
+        btn_send.setOnClickListener {
+            sendMessage()
         }
+
+        //scroll back to position when user clicks on text view
+        et_message.setOnClickListener {
+            GlobalScope.launch {
+                delay(100)
+                withContext(Dispatchers.Main) {
+                    rv_messages.scrollToPosition(adapter.itemCount - 1)
+
+                }
+            }
+        }
+
+        //recycler view used when add/input messages
+        adapter = MessagingAdapter()
+        rv_messages.adapter = adapter
+        //rv_messages.layoutManager = LinearLayoutManager()
+
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_assistant, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AssistantFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AssistantFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onStart() {
+        super.onStart()
+        //In case there are messages, scroll to bottom when re-opening app
+        GlobalScope.launch {
+            delay(100)
+            withContext(Dispatchers.Main) {
+                rv_messages.scrollToPosition(adapter.itemCount - 1)
+            }
+        }
+    }
+
+    //sending message to bot
+    private fun sendMessage() {
+        val message = et_message.text.toString()
+        val timeStamp = Time.timeStamp()
+
+        //receiving message from user
+        if (message.isNotEmpty()) {
+            //add message to local list
+            messagesList.add(Message(message, Constants.SEND_ID, timeStamp))
+            et_message.setText("")
+
+            //inserting/sending message to bot
+            adapter.insertMessage(Message(message, Constants.SEND_ID, timeStamp))
+            rv_messages.scrollToPosition(adapter.itemCount - 1)
+
+            //bot responding to users message
+            botResponse(message)
+        }
+    }
+
+    //bot responding to user
+    private fun botResponse(message: String) {
+        //response delay after 1000 milliseconds
+        val timeStamp = Time.timeStamp()
+        GlobalScope.launch {
+            delay(1000)
+
+            withContext(Dispatchers.Main) {
+                //bot receiving the response
+                val response = BotResponse.basicResponses(message)
+
+                //adding response to local list/recycler view to see the messages
+                messagesList.add(Message(response, Constants.RECEIVE_ID, timeStamp))
+
+                //inserts message into the adapter/message list from 0 to 1
+                adapter.insertMessage(Message(response, Constants.RECEIVE_ID, timeStamp))
+
+                //allow user to scroll to view past and recent messages
+                rv_messages.scrollToPosition(adapter.itemCount - 1)
+
+                //open google page
+                when (response) {
+                    Constants.OPEN_GOOGLE -> {
+                        val site = Intent(Intent.ACTION_VIEW)
+                        //google site
+                        site.data = Uri.parse("https://www.google.com/")
+                        startActivity(site)
+                    }
+                    //open google search
+                    Constants.OPEN_SEARCH -> {
+                        val site = Intent(Intent.ACTION_VIEW)
+                        val searchTerm: String? = message.substringAfterLast("search")
+                        //google search site
+                        site.data = Uri.parse("https://www.google.com/search?&q=$searchTerm")
+                        startActivity(site)
+                    }
+
                 }
             }
+        }
+    }
+
+    //bot will respond to user after 1000 milliseconds
+    private fun customBotMessage(message: String) {
+        GlobalScope.launch {
+            delay(1000)
+            withContext(Dispatchers.Main) {
+
+                //waiting to reply to user
+                val timeStamp = Time.timeStamp()
+
+                //replying back to user
+                messagesList.add(Message(message, Constants.RECEIVE_ID, timeStamp))
+                adapter.insertMessage(Message(message, Constants.RECEIVE_ID, timeStamp))
+
+                //scrolling up and down to view messages
+                rv_messages.scrollToPosition(adapter.itemCount - 1)
+            }
+        }
     }
 }
--- app/src/main/java/sheridan/jawedzak/autoedu/bottomNavbar/SearchFragment.kt
+++ app/src/main/java/sheridan/jawedzak/autoedu/bottomNavbar/SearchFragment.kt
@@ -1,33 +1,26 @@
 package sheridan.jawedzak.autoedu.bottomNavbar
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.*
 import sheridan.jawedzak.autoedu.R
+import sheridan.jawedzak.autoedu.dashLightSymbols.DataAdapter
+import sheridan.jawedzak.autoedu.dashLightSymbols.DatabaseModel
+import sheridan.jawedzak.autoedu.dashLightSymbols.SymbolDetail
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class SearchFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -35,22 +28,53 @@ class SearchFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                SearchFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
+    var list = ArrayList<DatabaseModel>()
+
+    private lateinit var database: FirebaseDatabase
+    private lateinit var reference: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        database = FirebaseDatabase.getInstance()
+        reference = database.getReference("Symbols")
+
+        getData()
+
+
+    }
+
+    fun onSymbolItemClicked(position: Int) {
+        //Toast.makeText(this, list[position].name, Toast.LENGTH_LONG).show()
+
+        var intent = Intent(this, SymbolDetail::class.java)
+        intent.putExtra("name", list[position].name)
+        intent.putExtra("trigger", list[position].trigger)
+        intent.putExtra("description", list[position].description)
+        intent.putExtra("solution", list[position].solution)
+        intent.putExtra("icon", list[position].icon)
+        startActivity(intent)
+    }
+
+    private fun getData(){
+
+        reference.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                Log.e("cancel", p0.toString())
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                for (data in p0.children) {
+                    var model = data.getValue(DatabaseModel::class.java)
+                    list.add(model as DatabaseModel)
                 }
+                if (list.size > 0) {
+                    val adapter = DataAdapter(list, this@SearchFragment)
+                    var f = findViewById<RecyclerView>(R.id.recyclerview)
+                    f.adapter = adapter
+                }
+
+            }
+        })
     }
 }
