e7386d56d9e011fefb11655df8fea57da5b3a16a
jawedzak <jawedzak@sheridancollege.ca>
2021-10-13 11:14:40 -0400

    - Removed history page and added feedback fragment instead

--- app/src/main/java/sheridan/jawedzak/autoedu/bottomNavbar/HistoryFragment.kt
+++ app/src/main/java/sheridan/jawedzak/autoedu/bottomNavbar/HistoryFragment.kt
@@ -1,41 +1,22 @@
 package sheridan.jawedzak.autoedu.bottomNavbar
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.fragment_history.*
 import sheridan.jawedzak.autoedu.R
-import sheridan.jawedzak.autoedu.dashLightSymbols.DataAdapter
-import sheridan.jawedzak.autoedu.dashLightSymbols.DatabaseModel
 import sheridan.jawedzak.autoedu.dashLightSymbols.OnSymbolClickListener
-import sheridan.jawedzak.autoedu.dashLightSymbols.SymbolDetail
-
 
 class HistoryFragment : Fragment(), OnSymbolClickListener {
 
-    //array list database
-    var list = ArrayList<DatabaseModel>()
-
-    //initialize variables
-    private lateinit var database: FirebaseDatabase
     private lateinit var reference: DatabaseReference
-    private lateinit var adapter: DataAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        //initialize database and symbols
-        database = FirebaseDatabase.getInstance()
-        reference = database.getReference("Symbols")
-
-        //retrieve data
-        getData()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -44,39 +25,41 @@ class HistoryFragment : Fragment(), OnSymbolClickListener {
         return inflater.inflate(R.layout.fragment_history, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_submit.setOnClickListener{
+
+            val editname = name.text.toString()
+            val editemail = email.text.toString()
+            val editfeedback = feedback.text.toString()
+
+            reference = FirebaseDatabase.getInstance().getReference("Users")
+            val User = User(editname, editemail, editfeedback)
+            reference.child(editname).setValue(User).addOnSuccessListener {
+
+                //clear fields
+                name.text.clear()
+                email.text.clear()
+                feedback.text.clear()
+
+                //show success message
+                Toast.makeText(
+                    activity,
+                    "Submitted",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }.addOnFailureListener{
+                Toast.makeText(
+                    activity,
+                    "could not submit",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
     override fun onSymbolItemClicked(position: Int) {
-        //retrieving symbol information
-        var intent = Intent(activity, SymbolDetail::class.java)
-        intent.putExtra("name", list[position].name)
-        intent.putExtra("trigger", list[position].trigger)
-        intent.putExtra("description", list[position].description)
-        intent.putExtra("solution", list[position].solution)
-        intent.putExtra("icon", list[position].icon)
-        startActivity(intent)
     }
 
-    private fun getData(){
-        //pulling data from firebase
-        reference.addValueEventListener(object : ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {
-                Log.e("cancel", p0.toString())
-            }
-
-            //calling recycler view to access symbols
-            override fun onDataChange(p0: DataSnapshot) {
-                list.clear()
-                for (data in p0.children) {
-                    var model = data.getValue(DatabaseModel::class.java)
-                    list.add(model as DatabaseModel)
-                }
-
-                //list of symbols
-                if (list.size > 0) {
-                    adapter = DataAdapter(list, this@HistoryFragment)
-                    history_recyclerview.adapter = adapter
-                    history_recyclerview.layoutManager = LinearLayoutManager(activity)
-                }
-            }
-        })
-    }
 }
---
+++ app/src/main/java/sheridan/jawedzak/autoedu/bottomNavbar/User.kt
@@ -0,0 +1,3 @@
+package sheridan.jawedzak.autoedu.bottomNavbar
+
+data class User(val name: String?, val email: String?, val feedback: String?)
